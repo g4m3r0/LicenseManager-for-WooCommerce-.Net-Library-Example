0ccd890cd3492c2575b5925f89001e644c51f5f7
g4m3r <contact@gsoftwarelab.com>
2020-05-14 17:48:50 +0200

    Initial Commit

---
+++ LicenseManagerExample/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LicenseManagerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.btnActivate = new System.Windows.Forms.Button();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.txtProductId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.Location = new System.Drawing.Point(88, 12);
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.Size = new System.Drawing.Size(147, 20);
+            this.txtLicense.TabIndex = 0;
+            this.txtLicense.Text = "PB2-YT7-Q7W-YPO";
+            // 
+            // btnActivate
+            // 
+            this.btnActivate.Location = new System.Drawing.Point(241, 9);
+            this.btnActivate.Name = "btnActivate";
+            this.btnActivate.Size = new System.Drawing.Size(75, 23);
+            this.btnActivate.TabIndex = 1;
+            this.btnActivate.Text = "Activate";
+            this.btnActivate.UseVisualStyleBackColor = true;
+            this.btnActivate.Click += new System.EventHandler(this.btnActivate_Click);
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(241, 38);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(75, 23);
+            this.btnValidate.TabIndex = 2;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // txtProductId
+            // 
+            this.txtProductId.Location = new System.Drawing.Point(88, 38);
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.Size = new System.Drawing.Size(147, 20);
+            this.txtProductId.TabIndex = 3;
+            this.txtProductId.Text = "1409";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "License Key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Product ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 70);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProductId);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.btnActivate);
+            this.Controls.Add(this.txtLicense);
+            this.Name = "Form1";
+            this.Text = "LicenseManagerExample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.Button btnActivate;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.TextBox txtProductId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LicenseManagerExample/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LicenseManager;
+
+namespace LicenseManagerExample
+{
+    public partial class Form1 : Form
+    {
+        private readonly LicenseManager.ApiHelper lcManager;
+        public Form1()
+        {
+            InitializeComponent();
+
+            const string host ="https://domain.com"; //Your wordpress host address
+            const string consumerKey = "ck_bfab6fdddc6bcd6892b8320d3f204d6547dd9c93"; //Your licensemanager.at consumer api key
+            const string consumerSecret = "cs_1dc161350edf85d795d8ee476eca54674573c947"; //Your licensemanager.at consumer api secret
+
+            lcManager = new LicenseManager.ApiHelper(host, consumerKey, consumerSecret, false);
+        }
+
+        private void btnActivate_Click(object sender, EventArgs e)
+        {
+            var activated = lcManager.ActivateLicense(txtLicense.Text, txtProductId.Text);
+            MessageBox.Show(activated.ToString(), @"Activated?");
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            var validated = lcManager.ValidateLicense(txtLicense.Text, txtProductId.Text);
+            MessageBox.Show(validated.ToString(), @"Activated?");
+        }
+    }
+}
